bbc4fa95a3c4f3e5fbe3c2c57bd3d106fc8301ac
Jon Shaw <Jonshaw@quickenloans.com>
2018-12-27 16:56:19 -0500

    Add project files.

---
+++ CODINGFUN/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CODINGFUN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days =  {"Monday", "Tueday", "Wedbesday", "THursday", "Firday", "Saturday", "Sunday"};
+        
+        foreach (var day in days )
+        {
+           
+            
+        }
+        
+        
+            Console.ReadKey();
+        }
+    }
+}
